112d534e47a5d1b673c4543906c781cd47b71ac0
SMOLLAN\sunils <sunil.swain@smollan.com>
2026-05-15 13:17:32 +0530

    app.js commited

--- src/app.js
+++ src/app.js
@@ -21,5 +21,13 @@ app.use(
 '/api/agent',
 agentRoutes
 );
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+
+    console.log(
+        `Server running on port ${PORT}`
+    );
+
+});
 module.exports = app;
